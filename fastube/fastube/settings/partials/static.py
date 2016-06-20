bda662ddd02908d717aa79bb9aa1625103b67c49
pn101 <philipnam101@gmail.com>
2016-06-20 19:37:20 +0900

    update staticfiles: add STATIC_ROOT

--- fastube/fastube/settings/partials/static.py
+++ fastube/fastube/settings/partials/static.py
@@ -6,9 +6,19 @@ from .base import BASE_DIR
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'fastube', 'static',)]
+STATICFILES_DIRS = [os.path.join(
+        BASE_DIR,
+        'fastube',
+        'static',
+)]
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(
+        PROJECT_ROOT_DIR,
+        'dist',
+        'static',
+)
 
 # Media Files
 MEDIA_ROOT = os.path.join(
