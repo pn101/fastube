c391135f1bc4cba53c0575206f7987cb359f4738
pn101 <philipnam101@gmail.com>
2016-06-20 16:25:02 +0900

    init users SignupView: and all relevant templates and urls

--- fastube/fastube/urls/__init__.py
+++ fastube/fastube/urls/__init__.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from users.views import *
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^signup/$', SignupView.as_view(), name='signup'),
 
 ] + static(settings.MEDIA_URL, documentroot=settings.MEDIA_ROOT)
