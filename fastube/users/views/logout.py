ee18835527b8fc6b96263e57355643c3b02faaa4
pn101 <philipnam101@gmail.com>
2016-06-21 15:53:35 +0900

    init messages.html: message added to LogoutView

--- fastube/users/views/logout.py
+++ fastube/users/views/logout.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.views.generic import View
 from django.contrib.auth import logout
 from django.shortcuts import redirect
@@ -7,4 +8,9 @@ class LogoutView(View):
 
     def get(self, request, *args, **kwargs):
         logout(request)
+        messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Thank you, come again',
+        )
         return redirect('user:login')
