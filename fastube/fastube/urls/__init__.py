0752c27458e690955ab22da17b6321fc0b044fba
pn101 <philipnam101@gmail.com>
2016-06-21 16:10:31 +0900

    refactor urls: moved users url to users app

--- fastube/fastube/urls/__init__.py
+++ fastube/fastube/urls/__init__.py
@@ -3,12 +3,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from users.views import *
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^', include('fastube.urls.user', namespace='user')),
+    url(r'^', include('users.urls', namespace='user')),
 
 ] + static(settings.MEDIA_URL, documentroot=settings.MEDIA_ROOT)
