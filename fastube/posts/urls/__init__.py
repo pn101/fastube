24151178119caed52103a09486b99fab873530c2
pn101 <philipnam101@gmail.com>
2016-06-22 15:45:40 +0900

    init PostCreateView

---
+++ fastube/posts/urls/__init__.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from posts.views import *
+
+
+urlpatterns = [
+    url(r'^new/$', PostCreateFormView.as_view(), name='new'),
+    url(r'^confirm/$', PostCreateConfirmView.as_view(), name='confirm'),
+
+]
