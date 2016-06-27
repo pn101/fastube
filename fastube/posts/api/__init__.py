734a347fef909222bdea78fa43586d9ee586129b
pn101 <philipnam101@gmail.com>
2016-06-27 15:15:26 +0900

    init PostCommentListAPIView

--- fastube/posts/api/__init__.py
+++ fastube/posts/api/__init__.py
@@ -1,1 +1,2 @@
 from .list import PostListAPIView
+from .comment import PostCommentListAPIView
---
+++ fastube/posts/serializers/comment.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from posts.models import Comment
+
+
+class CommentModelSerializer(serializers.ModelSerializer):
+
+    username = serializers.CharField(source='user.username')
+
+    class Meta:
+        model = Comment
+        fields = [
+            'username',
+            'content',
+        ]
