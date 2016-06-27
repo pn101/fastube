9c87b4abc1e7c8f4515be54bfed3871d0f16d253
pn101 <philipnam101@gmail.com>
2016-06-27 16:31:15 +0900

    update posts.js: refactor javascript, add ajax to comments

--- fastube/fastube/static/js/posts.js
+++ fastube/fastube/static/js/posts.js
@@ -3,9 +3,30 @@ $(document).ready(function() {
     var commentsCreateFormElement = $(commentsSectionElement).find('form');
     var commentsCreateFormInputContentElement = $(commentsCreateFormElement).find('input[name="content"]');
 
+    var postSlug = $('#comments-count').data('post-slug');
+    var commentAPIURL = '/api/posts/' + postSlug + '/comments/';
+
+    $.ajax({
+        url: commentAPIURL,
+        type: 'GET',
+        success: function(data) {
+            var commentsCount = data.length;
+            $('#comments-count').html(commentsCount);
+
+            data.forEach(function(comment) {
+                var commentContent = comment.content + ': posted by ' + comment.username;
+
+                var commentListElement = $('<li>').text(commentContent);
+                $('.comments-list').append(commentListElement);
+            });
+        },
+        error: function(data) {
+        },
+    })
+
     commentsCreateFormElement.submit(function() {
         var content = $(commentsCreateFormInputContentElement).val();
-        alert(content);
+        alert(commentAPIURL);
         $(commentsCreateFormInputContentElement).val('')
 
         return false;
