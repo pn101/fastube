57d2c86a7c8919939aa5f8b6f90f4752f767b2d1
pn101 <philipnam101@gmail.com>
2016-07-01 14:19:05 +0900

    update PostCommentListCreateAPIView

--- fastube/fastube/urls/api.py
+++ fastube/fastube/urls/api.py
@@ -5,6 +5,6 @@ from posts.api import *
 
 urlpatterns = [
     url(r'^posts/$', PostListAPIView.as_view(), name='list'),
-    url(r'^posts/(?P<slug>\w+)/comments/$', PostCommentListAPIView.as_view(), name='comments'),
+    url(r'^posts/(?P<slug>\w+)/comments/$', PostCommentListCreateAPIView.as_view(), name='comments'),
 
 ]
